27b569d15f420815a49521e757d089e7d264f7b0
melisa.ozturk <melisa.ozturk1234@gmail.com>
2023-01-16 19:13:33 +0300

    first commit

---
+++ Layout/Cell/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  Layout
+//
+//  Created by Melisa  Öztürk on 14.01.2023.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
